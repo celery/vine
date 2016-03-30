12aa3d17dea142664323816cd855ea499c8b78e2
Ionel Cristian Mărieș <contact@ionelmc.ro>
2016-03-30 16:30:45 +0300

    Add support for rasing with a specific traceback. Simplify the Promise a bit - seems the extra wrapping in Promise.throw is not necessary.

--- vine/five.py
+++ vine/five.py
@@ -50,3 +50,27 @@ def with_metaclass(Type, skip_attrs={'__dict__', '__weakref__'}):
         return Type(Class.__name__, Class.__bases__, attrs)
 
     return _clone_with_metaclass
+
+if PY3:
+    def reraise(tp, value, tb=None):
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
+
+else:
+    def exec_(_code_, _globs_=None, _locs_=None):
+        """Execute code in a namespace."""
+        if _globs_ is None:
+            frame = sys._getframe(1)
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
+            del frame
+        elif _locs_ is None:
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
+
+
+    exec_("""def reraise(tp, value, tb=None):
+    raise tp, value, tb
+""")
