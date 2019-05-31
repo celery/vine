9e5ccf23ff615f2cfff93016f1a40dc0ea9a100c
Mads Jensen <mje@inducks.org>
2019-05-31 10:36:28 +0200

    Drop support for Python 3.4

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ classes = """
     Programming Language :: Python :: 2
     Programming Language :: Python :: 2.7
     Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.4
     Programming Language :: Python :: 3.5
     Programming Language :: Python :: 3.6
     Programming Language :: Python :: 3.7
@@ -112,7 +111,7 @@ setuptools.setup(
     platforms=['any'],
     classifiers=classifiers,
     license='BSD',
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=[],
     tests_require=reqs('test.txt'),
     cmdclass={'test': pytest},
